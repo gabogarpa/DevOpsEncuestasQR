cdc4a0bea21c80425a284379952013dcd68e219e
gabogarpa <gabogarciapaz@gmail.com>
2025-11-30 21:49:42 -0600

    Update App.js
    importo respuestas

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import CrearEncuesta from './pages/CrearEncuesta';
 import GenerarQR from './pages/GenerarQR';
 import EscanearQR from './pages/EscanearQR';
 import FormularioEncuesta from './pages/FormularioEncuesta';
+import Respuestas from './pages/Respuestas';
 
 function App() {
   return (
